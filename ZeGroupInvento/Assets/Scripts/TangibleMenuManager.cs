13be051992465dd13f0aa6874696584db09f35b4
Léo Mendes <leo.mendes-melin@hotmail.fr>
2024-11-03 13:03:49 +0100

    ajout du menu tangible avec le switch de sous menu qui marchent pour guidelines + multi touc hfonctionne

--- ZeGroupInvento/Assets/Scripts/TangibleMenuManager.cs
+++ ZeGroupInvento/Assets/Scripts/TangibleMenuManager.cs
@@ -24,6 +24,16 @@ public class TangibleMenuManager : MonoBehaviour
     private bool readyForSecondTap;
     private bool isProcessingTouch;
 
+    // État actuel du menu
+    private enum MenuState
+    {
+        Global,
+        Guidelines,
+        GVertical,
+        GHorizontal
+    }
+    private MenuState currentState = MenuState.Global;
+
     private void Start()
     {
         parentCanvas = GetComponentInParent<Canvas>();
@@ -62,39 +72,120 @@ public class TangibleMenuManager : MonoBehaviour
         }
     }
 
+    public void OnHomeButtonClicked()
+    {
+        Debug.Log("Home button clicked");
+        if (currentActiveMenu != null)
+        {
+            Vector2 currentPos = currentActiveMenu.GetComponent<RectTransform>().anchoredPosition;
+            SwitchMenu(tangibleMenuGlobal, currentPos);
+            currentState = MenuState.Global;
+        }
+    }
+
+    private void SetupMenuButtons(GameObject menu)
+    {
+        if (menu == null) return;
+
+        Debug.Log("Setting up buttons for menu: " + menu.name);
+        Button[] buttons = menu.GetComponentsInChildren<Button>(true);
+
+        foreach (Button button in buttons)
+        {
+            Debug.Log("Found button: " + button.gameObject.name);
+            button.onClick.RemoveAllListeners();
+
+            switch (button.gameObject.name)
+            {
+                case "Home":
+                    Debug.Log("Setting up Home button");
+                    button.onClick.AddListener(OnHomeButtonClicked);
+                    break;
+                case "Guidelines":
+                    Debug.Log("Setting up Guidelines button");
+                    button.onClick.AddListener(OnGuidelinesButtonClicked);
+                    break;
+                case "GVertical":
+                    Debug.Log("Setting up GVertical button");
+                    button.onClick.AddListener(OnGVerticalButtonClicked);
+                    break;
+                case "GHorizontal":
+                    Debug.Log("Setting up GHorizontal button");
+                    button.onClick.AddListener(OnGHorizontalButtonClicked);
+                    break;
+            }
+        }
+    }
+
+    public void OnGuidelinesButtonClicked()
+    {
+        if (currentState == MenuState.Global)
+        {
+            ShowSubMenu();
+            currentState = MenuState.Guidelines;
+        }
+    }
+
+    public void OnGVerticalButtonClicked()
+    {
+        Debug.Log("GVertical clicked, current state: " + currentState);
+        if (currentState == MenuState.Guidelines)
+        {
+            ShowSubMenuFirstSelected();
+            currentState = MenuState.GVertical;
+        }
+        else if (currentState == MenuState.GHorizontal) // Ajout de cette condition
+        {
+            ShowSubMenuFirstSelected();
+            currentState = MenuState.GVertical;
+        }
+    }
+
+    public void OnGHorizontalButtonClicked()
+    {
+        Debug.Log("GHorizontal clicked, current state: " + currentState);
+        if (currentState == MenuState.Guidelines)
+        {
+            ShowSubMenuSecondSelected();
+            currentState = MenuState.GHorizontal;
+        }
+        else if (currentState == MenuState.GVertical) // Ajout de cette condition
+        {
+            ShowSubMenuSecondSelected();
+            currentState = MenuState.GHorizontal;
+        }
+    }
+
     private void ProcessTap(Vector2 tapPosition)
     {
         if (!readyForSecondTap)
         {
-            // Premier tap
             lastTapTime = Time.time;
             lastTapPosition = tapPosition;
             readyForSecondTap = true;
         }
         else
         {
-            // Second tap - vérifier si c'est un double tap valide
             float timeSinceLastTap = Time.time - lastTapTime;
             float distanceFromLastTap = Vector2.Distance(tapPosition, lastTapPosition);
 
             if (timeSinceLastTap <= doubleTapTimeThreshold && distanceFromLastTap <= tapDistanceThreshold)
             {
-                // Vérifier si le double tap est sur le menu existant
                 if (currentActiveMenu != null && IsPositionOverMenu(tapPosition))
                 {
                     HideMenu();
                 }
                 else
                 {
-                    // Double tap en dehors du menu -> afficher nouveau menu
                     ShowMenuGlobalAtPosition(tapPosition);
+                    currentState = MenuState.Global; // Réinitialiser l'état
                 }
             }
 
-            // Réinitialiser après le second tap
             readyForSecondTap = false;
         }
     }
+
 
     private bool IsPositionOverMenu(Vector2 screenPosition)
     {
@@ -129,10 +220,16 @@ public class TangibleMenuManager : MonoBehaviour
             Destroy(currentActiveMenu);
         }
 
+        Debug.Log("Creating new menu at position: " + screenPosition);
         currentActiveMenu = Instantiate(tangibleMenuGlobal, transform);
         menuPosition = screenPosition;
         PositionMenuAtScreenPoint(currentActiveMenu, screenPosition);
+
+        // Configure les boutons immédiatement après la création
+        SetupMenuButtons(currentActiveMenu);
     }
+
+
 
     private void PositionMenuAtScreenPoint(GameObject menu, Vector2 screenPoint)
     {
@@ -154,6 +251,7 @@ public class TangibleMenuManager : MonoBehaviour
 
     public void ShowSubMenu()
     {
+        Debug.Log("ShowSubMenu called");
         if (currentActiveMenu != null)
         {
             Vector2 currentPos = currentActiveMenu.GetComponent<RectTransform>().anchoredPosition;
@@ -186,13 +284,18 @@ public class TangibleMenuManager : MonoBehaviour
             Destroy(currentActiveMenu);
         }
 
+        Debug.Log("Switching to menu: " + newMenuPrefab.name);
         currentActiveMenu = Instantiate(newMenuPrefab, transform);
         RectTransform rectTransform = currentActiveMenu.GetComponent<RectTransform>();
         rectTransform.pivot = new Vector2(0.5f, 0.5f);
         rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
         rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
         rectTransform.anchoredPosition = position;
+
+        // Configure les boutons immédiatement après la création
+        SetupMenuButtons(currentActiveMenu);
     }
+
 
     public void HideMenu()
     {
@@ -201,6 +304,9 @@ public class TangibleMenuManager : MonoBehaviour
             Destroy(currentActiveMenu);
             currentActiveMenu = null;
         }
+
+        // Réinitialiser l'état du menu
+        currentState = MenuState.Global;
 
         // Réinitialiser l'état du double tap
         readyForSecondTap = false;
